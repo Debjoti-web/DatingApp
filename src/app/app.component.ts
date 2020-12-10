be5fd56dbf06545946c26f1e814af767ffe93000
debjoti <sengupta.debjoti95@gmail.com>
2020-12-11 00:28:18 +0530

    session 5 start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Dating-SPA';
+  values: any;
+  constructor(private http: HttpClient){
+
+  }
+  ngOnInit() {
+    this.getUsers();
+  }
+  getUsers(){
+    this.http.get("http://localhost:5000/api/v1/value/get").subscribe(response =>{
+      this.values=response;
+    }, error=>{
+      console.log(error);
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [	
     AppComponent,
-      ValueComponent
+      ValueComponent,
+      NavComponent
    ],
   imports: [
-    BrowserModule,HttpClientModule
+    BrowserModule,HttpClientModule, BrowserAnimationsModule,FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
